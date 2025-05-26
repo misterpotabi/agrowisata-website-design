c4d6f85947b08da8acc0e68702bcb4aaa80c53af
mister potabi <99777929+misterpotabi@users.noreply.github.com>
2025-05-26 17:15:18 +0700

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -55,16 +55,6 @@ export default function HeroSection({ onNavClick }: HeroSectionProps) {
           </div>
         </div>
       </div>
-
-{/*       {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-white animate-bounce">
-        <div className="flex flex-col items-center">
-          <span className="text-sm mb-2">Scroll untuk melihat lebih</span>
-          <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
-            <div className="w-1 h-3 bg-white rounded-full mt-2 animate-pulse"></div>
-          </div>
-        </div>
-      </div> */}
     </section>
   )
 }
